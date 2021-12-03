7d3194398ec28caf5ae7f6a99a6009c5028bd266
IvanAndreski <63723286+IvanAndreski@users.noreply.github.com>
2021-12-03 17:44:21 +0100

    added backend

---
+++ Домашна 2/tech prototype/backend/src/main/java/mk/finki/dians/backend/backend/repository/PlaceRepository.java
@@ -0,0 +1,33 @@
+package mk.finki.dians.backend.backend.repository;
+
+import mk.finki.dians.backend.backend.model.Place;
+import org.springframework.stereotype.Repository;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+@Repository
+public class PlaceRepository {
+    private final String databasePath = "./database";
+
+    public List<Place> findAllOfType(String type) {
+        List<Place> allPlaces = new ArrayList<>();
+        String path = String.format("%s/%s.csv", databasePath, type);
+        File file = new File(path);
+        try {
+            BufferedReader bf = new BufferedReader(new FileReader(file));
+            String line = bf.readLine();
+            while ((line = bf.readLine()) != null) {
+                String[] placeArray = line.split(",");
+                allPlaces.add(Place.placeFactory(placeArray, type));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return allPlaces;
+    }
+}
---
+++ Домашна 2/tech prototype/backend/src/main/java/mk/finki/dians/backend/backend/service/impl/PlaceServiceImpl.java
@@ -0,0 +1,54 @@
+package mk.finki.dians.backend.backend.service.impl;
+
+import mk.finki.dians.backend.backend.model.Place;
+import mk.finki.dians.backend.backend.repository.PlaceRepository;
+import mk.finki.dians.backend.backend.service.PlaceService;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PlaceServiceImpl implements PlaceService {
+    private final PlaceRepository placeRepository;
+
+    public PlaceServiceImpl(PlaceRepository placeRepository) {
+        this.placeRepository = placeRepository;
+    }
+
+    @Override
+    public List<Place> findAllOfType(String type) {
+        return placeRepository.findAllOfType(type);
+    }
+
+    @Override
+    public List<Place> findClosetFiveOfType(String type, Double myLon, Double myLat) {
+        return placeRepository.findAllOfType(type)
+                .stream()
+                .sorted(Comparator.comparing(place -> this.distance(myLon, myLat, place.getLon(), place.getLat())))
+                .limit(5)
+                .collect(Collectors.toList());
+    }
+
+    private double distance(double lon1, double lat1, double lon2, double lat2) {
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1))
+                * Math.sin(deg2rad(lat2))
+                + Math.cos(deg2rad(lat1))
+                * Math.cos(deg2rad(lat2))
+                * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+        return (dist);
+    }
+
+    private double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    private double rad2deg(double rad) {
+        return (rad * 180.0 / Math.PI);
+    }
+}
