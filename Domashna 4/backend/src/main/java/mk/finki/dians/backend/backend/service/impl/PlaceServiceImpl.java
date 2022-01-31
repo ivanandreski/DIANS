9bc3011cf9d141ff501e003e3dfeea76b22fc33d
Filip Danilovski <filip_danilovski@hotmail.com>
2022-01-31 21:06:09 +0100

    Add DistanceCalculator utility class

--- Domashna 4/backend/src/main/java/mk/finki/dians/backend/backend/service/impl/PlaceServiceImpl.java
+++ Domashna 4/backend/src/main/java/mk/finki/dians/backend/backend/service/impl/PlaceServiceImpl.java
@@ -3,6 +3,7 @@ package mk.finki.dians.backend.backend.service.impl;
 import mk.finki.dians.backend.backend.model.Place;
 import mk.finki.dians.backend.backend.repository.PlaceRepository;
 import mk.finki.dians.backend.backend.service.PlaceService;
+import mk.finki.dians.backend.backend.utils.DistanceCalculator;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -11,9 +12,11 @@ import java.util.stream.Collectors;
 @Service
 public class PlaceServiceImpl implements PlaceService {
     private final PlaceRepository placeRepository;
+    private final DistanceCalculator distanceCalculator;
 
-    public PlaceServiceImpl(PlaceRepository placeRepository) {
+    public PlaceServiceImpl(PlaceRepository placeRepository, DistanceCalculator distanceCalculator) {
         this.placeRepository = placeRepository;
+        this.distanceCalculator = distanceCalculator;
     }
 
     @Override
@@ -25,7 +28,7 @@ public class PlaceServiceImpl implements PlaceService {
     public List<Place> findClosestOfType(String type, Double myLon, Double myLat, Integer limit) {
         List<Place> places = placeRepository.findAllOfType(type)
                 .stream()
-                .sorted(Comparator.comparing(place -> this.distanceInKilometers(myLon, myLat, place.getLon(), place.getLat())))
+                .sorted(Comparator.comparing(place -> this.distanceCalculator.distanceInKilometers(myLon, myLat, place.getLon(), place.getLat())))
                 .collect(Collectors.toList());
 
         if(limit != null)
@@ -38,7 +41,7 @@ public class PlaceServiceImpl implements PlaceService {
     public List<Place> findClosestOfTypeInRadius(String type, Double myLon, Double myLat, Double radius) {
         return placeRepository.findAllOfType(type)
                 .stream()
-                .filter(place -> this.distanceInKilometers(myLon, myLat, place.getLon(), place.getLat()) < radius)
+                .filter(place -> this.distanceCalculator.distanceInKilometers(myLon, myLat, place.getLon(), place.getLat()) < radius)
                 .collect(Collectors.toList());
     }
 
@@ -59,25 +62,6 @@ public class PlaceServiceImpl implements PlaceService {
                 .stream()
                 .filter(place -> place.getId().equals(id))
                 .findFirst();
-    }
-
-    private double distanceInKilometers(double lon1, double lat1, double lon2, double lat2) {
-        // distance between latitudes and longitudes
-        double dLat = Math.toRadians(lat2 - lat1);
-        double dLon = Math.toRadians(lon2 - lon1);
-
-        // convert to radians
-        lat1 = Math.toRadians(lat1);
-        lat2 = Math.toRadians(lat2);
-
-        // apply formulae
-        double a = Math.pow(Math.sin(dLat / 2), 2) +
-                Math.pow(Math.sin(dLon / 2), 2) *
-                        Math.cos(lat1) *
-                        Math.cos(lat2);
-        double rad = 6371;
-        double c = 2 * Math.asin(Math.sqrt(a));
-        return rad * c;
     }
 
     public String convertCyrilicToLatin(String message){
